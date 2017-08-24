3797ebe5f12bffd085e3be5f195978ab82bd5b91
Josh <j0sh.oleary11@gmail.com>
2017-08-24 17:55:39 -0500

    Added FriendConf (Not finished)

--- src/io/github/scientiist/mcfriends/commands/FriendConf.java
+++ src/io/github/scientiist/mcfriends/commands/FriendConf.java
@@ -1,5 +1,55 @@
 package io.github.scientiist.mcfriends.commands;
 
-public class FriendConf {
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class FriendConf implements CommandExecutor {
+	
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String alias, String[] args) {
+		
+		
+		String command = cmd.getName().toLowerCase();
+		if (command.equals("friendconf")) {
+			
+			
+			if (!(sender instanceof Player)) {
+				
+				return false;
+			}
+			
+			Player player = (Player) sender;
+			if (args.length == 0) {
+				player.sendMessage(ChatColor.BLUE+"MCFriends configuration: /friendconf <action> [arguments]");
+				player.sendMessage(ChatColor.WHITE+"Actions: allowteleport");
+				return true;
+			} else {
+				
+				if (args[0].equalsIgnoreCase("allowteleport")) {
+						boolean allowTP;
+						if (args[1].equalsIgnoreCase("true")) {
+							allowTP = true;
+							
+						}
+						
+						if (args[1].equalsIgnoreCase("false")) {
+							allowTP = false;
+						}
+						
+						
+						player.sendMessage("");
+					
+					return true;
+				}
+				
+			}
+		}
+		return false;
+	}
 
 }
